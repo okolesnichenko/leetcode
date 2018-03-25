1aa3c049b218fb81dc25d19bfae236b6ccb5c352
world intro <prokaeser@hotmail.com>
2018-03-25 10:04:11 +0300

    up2

--- leetcodeeasy/Program.cs
+++ leetcodeeasy/Program.cs
@@ -35,7 +35,7 @@ namespace leetcodeeasy
                 {
                     if (J[jCnt] == S[sCnt])
                     {
-                        
+
                         count++;
                     }
                     jCnt++;
@@ -46,12 +46,113 @@ namespace leetcodeeasy
             return count;
         }
 
-        static void Main(string[] args) 
+        static int HammingDistance(int x, int y)
         {
-            Console.WriteLine(NumJewelsInStones("adE", "sdasdafaA"));
-            Console.WriteLine(ReverseString("hello"));
-            Console.ReadKey();
+            int count = 0;
+            int tmp;
 
+            if (y > x)
+            {
+                tmp = y;
+                y = x;
+                x = tmp;
+            }
+
+            while (x != 0)
+            {
+                if (x % 2 != y % 2)
+                {
+                    count++;
+                }
+                x /= 2;
+                y /= 2;
+            }
+
+            return count;
+        }
+
+        static bool JudgeCircle(string moves)
+        {
+            int i = 0;
+            int Ucnt = 0, Dcnt = 0, Rcnt = 0, Lcnt = 0;
+            while (i < moves.Length)
+            {
+                switch (moves[i])
+                {
+                    case 'U':
+                        Ucnt++;
+                        break;
+                    case 'D':
+                        Dcnt++;
+                        break;
+                    case 'R':
+                        Rcnt++;
+                        break;
+                    case 'L':
+                        Lcnt++;
+                        break;
+                    default:
+                        break;
+                }
+                i++;
+            }
+
+            return (Ucnt == Dcnt) && (Rcnt == Lcnt);
+        }
+
+        static IList<int> SelfDividingNumbers(int left, int right)
+        {
+            IList<int> self = new List<int>();
+            int buf = left;
+            int buf2 = left;
+            bool flag = false;
+            while (buf <= right)
+            {
+                buf2 = buf;
+                while (buf2 != 0)
+                {
+                    if (buf2 % 10 != 0)
+                    {
+                        if (buf % (buf2 % 10) == 0)
+                        {
+                            flag = true;
+                        }
+                        else
+                        {
+                            flag = false;
+                            break;
+                        }
+                    }
+                    else
+                    {
+                        flag = false;
+                        break;
+                    }
+                    buf2 /= 10;
+                }
+                
+                if (flag)
+                {
+                    self.Add(buf);
+                }
+                buf++;
+                flag = false;
+            }
+            return self;
+        }
+
+        static void Main(string[] args)
+        {
+            IList<int> aa = SelfDividingNumbers(1, 22);
+            foreach (int n in aa)
+            {
+                Console.Write("{0} ", n);
+            }
+            //Console.WriteLine(JudgeCircle("LRU"));
+            //Console.WriteLine(HammingDistance(13, 16));
+            //Console.WriteLine(NumJewelsInStones("adE", "sdasdafaA"));
+            //Console.WriteLine(ReverseString("hello"));
+            Console.ReadKey();
         }
     }
 }
